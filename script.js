22c21221aa0fe29c4ef699eca43f8b0d8b077c51
niksiks23 <nikitaradivonik2007@gmail.com>
2026-03-03 17:26:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,105 @@
+$(document).ready(function () {
+
+    const symbols = ["⚽","🎧","🎮","🐨","💲","🔥","👑","🏰"];
+
+    let firstCard = null;
+    let secondCard = null;
+    let lockBoard = false;
+    let gameStarted = false;
+
+    function shuffle(array) {
+        return array.sort(() => Math.random() - 0.5);
+    }
+
+    function createBoard() {
+        $(".game-board").empty();
+
+        const doubled = shuffle([...symbols, ...symbols]);
+
+        $.each(doubled, function (index, symbol) {
+
+            const card = $(`
+                <div class="card" data-symbol="${symbol}">
+                    <div class="card-inner">
+                        <div class="card-face card-front"></div>
+                        <div class="card-face card-back">${symbol}</div>
+                    </div>
+                </div>
+            `);
+
+            $(".game-board").append(card);
+        });
+        $(".card").addClass("flipped");
+    }
+
+    function startGame() {
+        firstCard = null;
+        secondCard = null;
+        lockBoard = false;
+        gameStarted = true;
+        $(".card").removeClass("flipped matched");
+        $("#controlBtn").text("Finish");
+    }
+
+    function finishGame() {
+        $(".card").addClass("flipped");
+        $(".card").removeClass("matched");
+        $(".card").off("click");
+
+        gameStarted = false;
+        $("#controlBtn").text("Start");
+    }
+
+    function resetTurn() {
+        firstCard = null;
+        secondCard = null;
+        lockBoard = false;
+    }
+
+    function checkWin() {
+        if ($(".matched").length === $(".card").length) {
+            setTimeout(function () {
+                alert("!YOU WIN!");
+                finishGame();
+            }, 500);
+        }
+    }
+
+    $(".game-board").on("click", ".card", function () {
+        if (!gameStarted) return;
+        if (lockBoard) return;
+        if ($(this).hasClass("flipped") || $(this).hasClass("matched")) return;
+
+        $(this).addClass("flipped");
+
+        if (!firstCard) {
+            firstCard = $(this);
+            return;
+        }
+
+        secondCard = $(this);
+        lockBoard = true;
+
+        if (firstCard.data("symbol") === secondCard.data("symbol")) {
+            firstCard.addClass("matched");
+            secondCard.addClass("matched");
+            resetTurn();
+            checkWin();
+        } else {
+            setTimeout(function () {
+                firstCard.removeClass("flipped");
+                secondCard.removeClass("flipped");
+                resetTurn();
+            }, 2000);
+        }
+    });
+
+    $("#controlBtn").click(function () {
+        if (!gameStarted) {
+            startGame();
+        } else {
+            finishGame();
+        }
+    });
+    createBoard();
+});
